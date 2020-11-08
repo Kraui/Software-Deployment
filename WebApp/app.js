b6454eef24ffe0eba02bf2fce1a508dfa6b9b392
Christoph Enke <110046@fhwn.ac.at>
2020-11-08 19:36:40 +0100

    app insights

--- WebApp/app.js
+++ WebApp/app.js
@@ -2,6 +2,11 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+let appInsights = require("applicationinsights");
+appInsights
+  .setup("5d8938b0-d7b6-425c-beb1-0842fe5f81c4")
+  .start()
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
   res.send('test')
