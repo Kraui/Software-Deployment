3e87e9bf3dd59bf341a08bdca0dca912bd4065ea
Christoph Enke <110046@fhwn.ac.at>
2020-11-09 21:20:56 +0100

    jade update

--- WebApp/app.js
+++ WebApp/app.js
@@ -1,15 +1,16 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT||3000;
 
 let appInsights = require("applicationinsights");
 appInsights
   .setup("5d8938b0-d7b6-425c-beb1-0842fe5f81c4")
   .start()
 
+app.set("view engine", "jade");
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-  res.send('test')
+  res.render("index");
 })
 
 app.listen(port, () => {
